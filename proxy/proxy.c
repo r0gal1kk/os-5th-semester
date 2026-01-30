f1a96aaffe518bd9799b56d0884b604c5783900e
r0gal1kk <105783765+r0gal1kk@users.noreply.github.com>
2026-01-30 08:10:03 +0700

    правки для proxy.c

--- proxy/proxy.c
+++ proxy/proxy.c
@@ -74,43 +74,99 @@ static int connect_to_host(const char *host, const char *port) {
     return fd;
 }
 
-static const char *find_header(
+typedef struct {
+    const char *value;
+    size_t value_len;
+} header_value_t;
+
+static header_value_t find_header(
     struct phr_header *headers,
     size_t num_headers,
     const char *name
 ) {
+    header_value_t res = { NULL, 0 };
+
     for (size_t i = 0; i < num_headers; i++) {
-        if (strncasecmp(headers[i].name, name, headers[i].name_len) == 0)
-            return headers[i].value;
+        if (headers[i].name_len == strlen(name) &&
+            strncasecmp(headers[i].name, name, headers[i].name_len) == 0) {
+
+            res.value = headers[i].value;
+            res.value_len = headers[i].value_len;
+            return res;
+        }
     }
-    return NULL;
+    return res;
 }
 
-static void proxy_relay(int a, int b) {
+static void proxy_relay(int client_fd, int server_fd) {
     struct pollfd fds[2];
-    char buf[BUF_SIZE];
+    char buffer[BUF_SIZE];
+    int stop = 0;
 
-    fds[0].fd = a;
+    fds[0].fd = client_fd;     
     fds[0].events = POLLIN;
-
-    fds[1].fd = b;
+    fds[1].fd = server_fd;     
     fds[1].events = POLLIN;
 
-    while (1) {
-        int r = poll(fds, 2, -1);
-        if (r <= 0)
-            break;
-
-        if (fds[0].revents & POLLIN) {
-            ssize_t n = recv(a, buf, sizeof(buf), 0);
-            if (n <= 0) break;
-            send(b, buf, n, 0);
+    while (!stop) {
+        int poll_result = poll(fds, 2, -1);
+        if (poll_result < 0) {
+            if (errno == EINTR) continue;
+            stop = 1;
+            continue;
+        }
+        if (poll_result == 0) {
+            stop = 1;
+            continue;
         }
 
-        if (fds[1].revents & POLLIN) {
-            ssize_t n = recv(b, buf, sizeof(buf), 0);
-            if (n <= 0) break;
-            send(a, buf, n, 0);
+        // клиент -> сервер
+        if (!stop && (fds[0].revents & POLLIN)) {
+            ssize_t bytes_received = recv(client_fd, buffer, sizeof(buffer), 0);
+            if (bytes_received <= 0) {
+                stop = 1;
+                continue;
+            }
+
+            size_t remaining = (size_t)bytes_received;
+            size_t sent_so_far = 0;
+
+            while (sent_so_far < remaining && !stop) {
+                ssize_t sent_this_time = send(server_fd, buffer + sent_so_far, remaining - sent_so_far, 0);
+
+                if (sent_this_time <= 0) {
+                    stop = 1;
+                } else {
+                    sent_so_far += (size_t)sent_this_time;
+                }
+            }
+        }
+
+        // сервер -> клиент
+        if (!stop && (fds[1].revents & POLLIN)) {
+            ssize_t bytes_received = recv(server_fd, buffer, sizeof(buffer), 0);
+            if (bytes_received <= 0) {
+                stop = 1;
+                continue;
+            }
+
+            size_t remaining = (size_t)bytes_received;
+            size_t sent_so_far = 0;
+
+            while (sent_so_far < remaining && !stop) {
+                ssize_t sent_this_time = send(client_fd, buffer + sent_so_far, remaining - sent_so_far, 0);
+
+                if (sent_this_time <= 0) {
+                    stop = 1;
+                } else {
+                    sent_so_far += (size_t)sent_this_time;
+                }
+            }
+        }
+
+        if ((fds[0].revents & (POLLHUP | POLLERR)) ||
+            (fds[1].revents & (POLLHUP | POLLERR))) {
+            stop = 1;
         }
     }
 }
@@ -120,9 +176,9 @@ static void *client_thread(void *arg) {
     int client_fd = ctx->client_fd;
     free(ctx);
 
-    pthread_detach(pthread_self());
+    char buf[32768];
+    size_t buf_used = 0;
 
-    char buf[BUF_SIZE];
     struct phr_header headers[MAX_HEADERS];
     size_t num_headers = MAX_HEADERS;
 
@@ -130,30 +186,51 @@ static void *client_thread(void *arg) {
     int minor_version;
     size_t method_len, path_len;
 
-    ssize_t n = recv(client_fd, buf, sizeof(buf), 0);
-    if (n <= 0) {
-        close(client_fd);
-        return NULL;
+    int pret;
+
+    // Читаем заголовки до тех пор, пока не распарсим полностью
+    while (1) {
+        ssize_t r = recv(client_fd, buf + buf_used, sizeof(buf) - buf_used - 1, 0);
+
+        if (r <= 0) {
+            close(client_fd);
+            return NULL;
+        }
+
+        buf_used += r;
+
+        pret = phr_parse_request(buf, buf_used, &method, 
+            &method_len, &path, &path_len, 
+            &minor_version, headers, &num_headers, 0);
+
+        if (pret > 0) {
+            //success
+            break;
+        }
+
+        if (pret == -1) {
+            // некорректный запрос
+            const char *msg = "HTTP/1.0 400 Bad Request\r\n\r\n";
+            send(client_fd, msg, strlen(msg), 0);
+            close(client_fd);
+            return NULL;
+        }
+
+        if (pret == -2) {
+            // нужно больше данных
+            if (buf_used >= sizeof(buf) - 1024) {
+                const char *msg = "HTTP/1.0 413 Request Entity Too Large\r\n\r\n";
+                send(client_fd, msg, strlen(msg), 0);
+                close(client_fd);
+                return NULL;
+            }
+            // продолжаем чтение
+            continue;
+        }
     }
 
-    int pret = phr_parse_request(
-        buf, n,
-        &method, &method_len,
-        &path, &path_len,
-        &minor_version,
-        headers, &num_headers,
-        0
-    );
-
-    if (pret <= 0) {
-        const char *msg = "HTTP/1.0 400 Bad Request\r\n\r\n";
-        send(client_fd, msg, strlen(msg), 0);
-        close(client_fd);
-        return NULL;
-    }
-
-    const char *host = find_header(headers, num_headers, "Host");
-    if (!host) {
+    header_value_t host_hdr = find_header(headers, num_headers, "Host");
+    if (!host_hdr.value) {
         const char *msg = "HTTP/1.0 400 Bad Request\r\n\r\nNo Host header";
         send(client_fd, msg, strlen(msg), 0);
         close(client_fd);
@@ -162,7 +239,7 @@ static void *client_thread(void *arg) {
 
     char host_buf[256];
     snprintf(host_buf, sizeof(host_buf), "%.*s",
-             (int)headers[0].value_len, host);
+             (int)host_hdr.value_len, host_hdr.value);
 
     int server_fd = connect_to_host(host_buf, "80");
     if (server_fd < 0) {
@@ -172,7 +249,8 @@ static void *client_thread(void *arg) {
         return NULL;
     }
 
-    send(server_fd, buf, n, 0);
+    send(server_fd, buf, pret, 0);
+
     proxy_relay(client_fd, server_fd);
 
     close(server_fd);
@@ -197,10 +275,15 @@ int main(void) {
         client_arg_t *arg = malloc(sizeof(*arg));
         arg->client_fd = client_fd;
 
+
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
+        pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
         pthread_t tid;
         if (pthread_create(&tid, NULL, client_thread, arg) != 0) {
             close(client_fd);
             free(arg);
         }
+        pthread_attr_destroy(&attr);
     }
 }
